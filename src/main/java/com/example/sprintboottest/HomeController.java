92ed1534d8047b03e3bb765b9e9cc6dc522ad346
Romulo Xavier <romulo.xavier@catskillet.com>
2023-05-04 09:19:46 -0300

    feat: criando uma controller para acessar index html.

---
+++ src/main/java/com/example/sprintboottest/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.sprintboottest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homeApp(Model model, Model modelMessage)
+    {
+        model.addAttribute("message", "essa mensagem foi injetada via model.");
+        modelMessage.addAttribute("subTitle", "testando o segundo model.");
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/sprintboottest/SprintBootTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootTestApplication.class, args);
+	}
+
+}
