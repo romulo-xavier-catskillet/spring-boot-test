5c5793c02077e74057a952055568d87d0be343be
Romulo Xavier <romulo.xavier@catskillet.com>
2023-05-05 15:08:44 -0300

    feat: Iniciando com spring security. Iniciando com a criação e configuração da tela de login. Ajustando o front-end da pagina create dos empregos.

--- src/main/java/com/example/sprintboottest/controllers/HomeController.java
+++ src/main/java/com/example/sprintboottest/controllers/HomeController.java
@@ -1,17 +1,14 @@
 package com.example.sprintboottest.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String homeApp(Model model, Model modelMessage)
+    public String login()
     {
-        model.addAttribute("message", "essa mensagem foi injetada via model.");
-        modelMessage.addAttribute("subTitle", "testando o segundo model.");
-        return "index";
+        return "login";
     }
 }
